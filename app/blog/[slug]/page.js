7f3a917d000461138391184a1d2d988944e78a0c
tarandslcon3@gmail.com <tarandslcon3@gmail.com>
2026-06-23 18:29:37 -0400

    Fix: add use client to blog pages for Next.js 14 compatibility

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,3 +1,4 @@
+'use client'
 import { notFound } from 'next/navigation'
 import Link from 'next/link'
 import { posts, getPostBySlug } from '../posts/data'
--- app/blog/page.js
+++ app/blog/page.js
@@ -1,3 +1,4 @@
+'use client'
 import Link from 'next/link'
 import { posts } from './posts/data'
 import Nav from '@/components/Nav'
